e46401a2f1c5393d335c777ebdbbcbc82ae2604f
msatyakanth <marrivadasatyakanthreddy@gmail.com>
2023-05-22 22:28:04 +0530

    First Commit

---
+++ ShareProect/src/main/java/com/satyakanth/ShareProect/App.java
@@ -0,0 +1,13 @@
+package com.satyakanth.ShareProect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
